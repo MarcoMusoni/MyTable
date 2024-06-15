f41763acd4a41dcd4878c82f2c801de230ae037c
MarcoMusoni <marco.musoni@studenti.unimi.it>
2024-06-15 14:00:24 +0200

    Tables scripts

---
+++ routes/tables.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var parser = express.json();
+
+/* GET reservations for the day */
+router.get('/', function (req, res, next) {
+  if (req.query.selectedDate) {
+    console.log('GET tables on: ' + req.query.selectedDate);
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+/* POST table reservation */
+router.post('/', function (req, res, next) {
+  var bookingInfo = JSON.parse(req.body);
+  if (bookingInfo.user && bookingInfo.selectedDate && bookingInfo.table) {
+    console.log('User: ' + JSON.stringify(bookingInfo.user));
+    console.log('Date: ' + bookingInfo.selectedDate);
+    console.log('Table: ' + JSON.stringify(bookingInfo.table));
+  } else {
+    res.status(400);
+    res.send();
+  }
+});
+
+module.exports = router;
