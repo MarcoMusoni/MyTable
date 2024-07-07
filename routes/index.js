b308b15dedf2bc152846ab8021029f599780de12
MarcoMusoni <marco.musoni@studenti.unimi.it>
2024-07-07 16:06:36 +0200

    Login done

--- routes/index.js
+++ routes/index.js
@@ -6,26 +6,4 @@ router.get("/", function (req, res, next) {
   res.render("index");
 });
 
-/* Login procedure */
-router.post("/login", function (req, res, next) {
-  let resStatus = 200;
-  let loginStatus = {
-    missingData: false
-  };
-
-  var body = req.body;
-  console.info(
-    new Date().toLocaleString() + " | Login attempted: " + JSON.stringify(body)
-  );
-
-  if (body.usrMail && body.usrPsw) {
-    // var user = retrieveUserWithMail(body.usrMail);
-  } else {
-    resStatus = 400;
-    loginStatus.missingData = true;
-  }
-
-  res.status(resStatus).send(loginStatus);
-});
-
 module.exports = router;
--- routes/tables.js
+++ routes/tables.js
@@ -3,19 +3,15 @@ var router = express.Router();
 
 /* GET reservations for the day */
 router.get('/', function (req, res, next) {
-  if (req.query.selectedDate) {
-    console.log('GET tables on: ' + req.query.selectedDate);
-    res.render('index', { title: 'Tables' })
-  } else {
-    res.sendStatus(400);
-  }
+  console.log('Tables GETTED');
+  res.status(200).send();
 });
 
 /* POST table reservation */
 router.post('/', function (req, res, next) {
-  
+
   console.log('Body: ' + JSON.stringify(req.body));
-  
+
   if (req.body.usrPsw && req.body.usrMail) {
     console.log(req.body.usrPsw);
     console.log(req.body.usrMail);
